da74abe3cff7eabc46404f1627fbfbce52a3af27
AuroraWXZ <wang6171@purdue.edu>
2024-04-23 02:13:37 -0400

    MLP Classification

--- model.py
+++ model.py
@@ -11,189 +11,27 @@ import numpy as np
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 np.random.seed(577)
 
-def label_encode(df, cols):
-    feature_mappings = {}
-    df_feature = pd.DataFrame()
-    for column in cols:
-        categories = df[column].unique()
-        label_mapping = {}
-        encoded_labels = []
-        label_count = 1
-
-        for category in categories:
-            label_mapping[category] = label_count
-            label_count += 1
-
-        # print(column)
-        # print(df[column].isna().sum())
-        # print(label_mapping)
-        for value in df[column]:
-            encoded_labels.append(label_mapping[value])
-
-        feature_mappings[column] = label_mapping
-        df_feature[column] = encoded_labels
-
-    return feature_mappings, df_feature
-
-def train_embedding(df, feature_cols, label_cols):
-    data = {}
-    encoded_data = pd.DataFrame()
-    data, encoded_data = label_encode(df, feature_cols)
-
-    labels = {}
-    encoded_labels = pd.DataFrame()
-    labels, encoded_labels = label_encode(df, label_cols)
-
-    for column in label_cols:
-        encoded_data[column] = encoded_labels[column]
-
-    return encoded_data, data, labels
-
-def test_embedding(df, features, labels):
-    new_data = pd.DataFrame()
-    for column in features.keys():
-        encoded_labels = []
-        for value in df[column]:
-            if features[column].get(value) is not None:
-                encoded_labels.append(features[column][value])
-            else:
-                features[column][value] = len(features[column])+1
-                encoded_labels.append(features[column][value])
-            # print(value)
-            # print(features[column])
-        new_data[column] = encoded_labels
-
-    for column in labels.keys():
-        encoded_labels = []
-        for value in df[column]:
-            if labels[column].get(value) is not None:
-                encoded_labels.append(labels[column][value])
-            else:
-                labels[column][value] = len(labels[column])+1
-                encoded_labels.append(labels[column][value])
-            # print(value)
-            # print(labels[column])
-        new_data[column] = encoded_labels
-
-    return new_data
-                    
-
-    
-# Custom Dataset class
-class TrafficDataset(Dataset):
-    def __init__(self, df, feature_cols, label_cols):
-        """
-        Args:
-            dataframe (DataFrame): Pandas DataFrame containing the data.
-            feature_cols (list): List of column names to be used as features.
-            label_cols (list): List of column names to be used as labels.
-        """
-        self.features = df[feature_cols]
-        self.labels = df[label_cols]
-    
-    def __len__(self):
-        return len(self.features)
-    
-    def __getitem__(self, idx):
-        feature = torch.tensor(self.features.iloc[idx].values, dtype=torch.float)
-        label = torch.tensor(self.labels.iloc[idx].values, dtype=torch.float)
-
-        # print(feature)
-        return feature, label
-
-
-
-class TrafficCNN(nn.Module):
-    def __init__(self, num_features, num_classes):
-        super(TrafficCNN, self).__init__()
-        self.conv1 = nn.Conv1d(num_features, 16, kernel_size=3, stride=1, padding=1)
-        self.conv2 = nn.Conv1d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.pool = nn.MaxPool1d(2, 2)
-        self.fc1 = nn.Linear(32 * 1, 32)  # Adjust the input features to match your data
-        self.fc2 = nn.Linear(32, 84)
-        self.fc3 = nn.Linear(84, num_classes)
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, activation):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        if activation == "relu":
+            self.activation = F.relu
+        elif activation == "tanh":
+            self.activation = F.tanh
+        elif activation == "logistic":
+            self.activation = torch.sigmoid
 
     def forward(self, x):
-        # Add sequence of convolutional and max pooling layers
-        x = F.relu(self.conv1(x))
-        # print("After conv1:", x.size())
-        x = F.relu(self.conv2(x))
-        # print("After conv2:", x.size())
-        x = x.view(-1, 32 * 1)  # Flatten the tensor
-        # print("After view:", x.size())
-        x = F.relu(self.fc1(x))
-        # print("After fc1:", x.size())
-        x = F.relu(self.fc2(x))
-        # print("After fc2:", x.size())
-        x = self.fc3(x)
-        # print("After fc3:", x.size())
+        x = self.fc1(x)
+        x = self.activation(x)
+        x = self.fc2(x)
         return x
 
-class RNNClassifier(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, num_classes):
-        super(RNNClassifier, self).__init__()
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
-
-    def forward(self, x):
-        # Initialize hidden and cell states
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
         
-        # Forward propagate the RNN
-        out, _ = self.rnn(x, h0)
-        
-        # Pass the output of the last time step to the classifier
-        out = self.fc(out[:, -1, :])
-        return out
-
-
-def train_model(train_loader, valid_loader, model, criterion, optimizer, num_epochs=25):
-    for epoch in range(1):
-        model.train()  # Set the model to training mode
-        running_loss = 0.0
-        
-        for i, (feature, label) in enumerate(train_loader):
-            # Zero the parameter gradients
-            feature, label = feature.to(device), label.to(device)
-            feature = feature.unsqueeze(2)
-
-            optimizer.zero_grad()
-            outputs = model(feature)
-            print(outputs)
-            loss = criterion(outputs, label)
-            
-            # Backward pass and optimize
-            loss.backward()
-            optimizer.step()
-            
-            # Print statistics
-            running_loss += loss.item()
-            if i % 5 == 4:    # Print every 100 mini-batches
-                print(f'Epoch {epoch + 1}, Batch {i + 1}, Loss: {running_loss / 100:.4f}')
-                running_loss = 0.0
-
-    # Validation loss
-    correct = 0
-    total = 0
-    model.eval()  # Set the model to evaluation mode
-    with torch.no_grad():
-        for feature, label in valid_loader:
-            feature, label = feature.to(device), label.to(device)
-            feature = feature.unsqueeze(2)
-            outputs = model(feature)
-            print(outputs)
-            _, predicted = torch.max(outputs.data, 1)
-            total += label.size(0)
-            print(predicted)
-            correct += (predicted == label).sum().item()
-
-    print(f'Validation Accuracy: {100 * correct / total:.2f}%')
-
-    print('Finished Training')
-        
-
+def encode_targets(df, label_encoders, target):
+    return label_encoders[target].transform(df[[target]])
 
 
 
@@ -213,44 +51,185 @@ test_df = pd.read_csv(data_folder + "test_set.csv")
 # Read the valid_set.csv file
 valid_df = pd.read_csv(data_folder + "valid_set.csv")
 
-train_data, features, labels = train_embedding(train_df, feature_cols, label_cols)
-test_data = test_embedding(test_df, features, labels)
-valid_data = test_embedding(valid_df, features, labels)
+df = pd.concat([train_df, test_df, valid_df])
+
+data_encoder = OneHotEncoder(sparse=False)
+data_encoder.fit(df[feature_cols])
+
+train_data = data_encoder.transform(train_df[feature_cols])
+test_data = data_encoder.transform(test_df[feature_cols])
+valid_data = data_encoder.transform(valid_df[feature_cols])
 
 
-# Create datasets
-train_dataset = TrafficDataset(train_data, feature_cols, label_cols)
-test_dataset = TrafficDataset(test_data, feature_cols, label_cols)
-valid_dataset = TrafficDataset(valid_data, feature_cols, label_cols)
+label_encoders = {}
+
+for label in label_cols:
+    label_encoder = OneHotEncoder(sparse=False)
+    label_encoder.fit(df[[label]])
+    label_encoders[label] = label_encoder
 
 
-# Create dataloaders
-batch_size = 8
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
-valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
+train_labels = {label: encode_targets(train_df, label_encoders, label) for label in label_cols}
+valid_labels = {label: encode_targets(valid_df, label_encoders, label) for label in label_cols}
+test_labels = {label: encode_targets(test_df, label_encoders, label) for label in label_cols}
 
 # Number of features after one-hot encoding
-num_features = train_dataset.features.shape[1] # You need to calculate this based on your one-hot encoded data
-# # Number of classes for the classification task
-num_classes = train_dataset.labels.shape[1] # You need to define this based on your label data
+num_features = train_data.shape[1] # You need to calculate this based on your one-hot encoded data
+# Number of classes for the classification task
+num_classes = len(train_labels) # You need to define this based on your label data
 
-    
 
 # Instantiate the model
 
-model = TrafficCNN(num_features, num_classes).to(device)
+# print(train_labels['DAMAGE'].shape)
 
-# input_size = 6  # Number of features
-# hidden_size = 128  # Number of features in the hidden state
-# num_layers = 2  # Number of stacked RNN layers
-# num_classes = 4  # Number of output classes
-
-# # Initialize the model
-# model = RNNClassifier(input_size, hidden_size, num_layers, num_classes).cuda()
+model_CT = MLP(num_features, 64, train_labels['CRASH_TYPE'].shape[1], "logistic").to(device)
+model_D = MLP(num_features, 200, train_labels['DAMAGE'].shape[1], "logistic").to(device)
+model_MSI= MLP(num_features, 64, train_labels['MOST_SEVERE_INJURY'].shape[1], "logistic").to(device)
+model_IT = MLP(num_features, 64, train_labels['INJURIES_TOTAL'].shape[1], "relu").to(device)
 
 # Define loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+criterion_CT = nn.CrossEntropyLoss()
+criterion_D  = nn.CrossEntropyLoss()
+criterion_MSI = nn.CrossEntropyLoss()
+criterion_IT = nn.CrossEntropyLoss()
+optimizer_CT = torch.optim.Adam(model_CT.parameters(), lr=0.001)
+optimizer_D = torch.optim.Adam(model_D.parameters(), lr=0.0001)
+optimizer_MSI = torch.optim.Adam(model_MSI.parameters(), lr=0.0001)
+optimizer_IT = torch.optim.Adam(model_IT.parameters(), lr=0.0001)
 
-train_model(train_loader, valid_loader, model, criterion, optimizer, num_epochs=25)
+for epoch in range(10):
+    for i in range(train_data.shape[0]):
+        optimizer_CT.zero_grad()
+        optimizer_D.zero_grad()
+        optimizer_MSI.zero_grad()
+        optimizer_IT.zero_grad()
+
+        line = torch.tensor(train_data[i], dtype=torch.float).to(device)
+        outputs_CT = model_CT(line)
+        outputs_D = model_D(line)
+        outputs_MSI = model_MSI(line)
+        outputs_IT = model_IT(line)
+
+        # print(outputs_D.shape)
+
+        loss_CT = criterion_CT(outputs_CT, torch.tensor(train_labels['CRASH_TYPE'][i], dtype=torch.float).to(device))
+        loss_D = criterion_D(outputs_D, torch.tensor(train_labels['DAMAGE'][i], dtype=torch.float).to(device))
+        loss_MSI = criterion_MSI(outputs_MSI, torch.tensor(train_labels['MOST_SEVERE_INJURY'][i], dtype=torch.float).to(device))
+        loss_IT = criterion_IT(outputs_IT, torch.tensor(train_labels['INJURIES_TOTAL'][i], dtype=torch.float).to(device))
+
+        loss_CT.backward()
+        loss_D.backward()
+        loss_MSI.backward()
+        loss_IT.backward()
+
+        optimizer_CT.step()
+        optimizer_D.step()
+        optimizer_MSI.step()
+        optimizer_IT.step()
+
+        # if (i+1) % 50 == 0:
+            # print(f'Epoch [{epoch+1}/10], Loss: {loss_CT.item()}')
+            # print(f'Epoch [{epoch+1}/10], Loss: {loss_D.item()}')
+            # print(f'Epoch [{epoch+1}/10], Loss: {loss_MSI.item()}')
+            # print(f'Epoch [{epoch+1}/10], Loss: {loss_IT.item()}')
+            # print("--------------- Line ---------------")
+
+model_CT.eval()
+model_D.eval()
+model_MSI.eval()
+model_IT.eval()
+
+total_loss = [0, 0, 0, 0]
+correct = [0, 0, 0, 0]
+
+with torch.no_grad():
+    for i in range(valid_data.shape[0]):
+        line = torch.tensor(valid_data[i], dtype=torch.float).to(device)
+        outputs_CT = model_CT(line)
+        outputs_D = model_D(line)
+        outputs_MSI = model_MSI(line)
+        outputs_IT = model_IT(line)
+
+        label_CT = torch.tensor(valid_labels['CRASH_TYPE'][i], dtype=torch.float).to(device)
+        loss_CT = criterion_CT(outputs_CT, label_CT)
+
+        label_D = torch.tensor(valid_labels['DAMAGE'][i], dtype=torch.float).to(device)
+        loss_D = criterion_D(outputs_D, label_D)
+
+        label_MSI = torch.tensor(valid_labels['MOST_SEVERE_INJURY'][i], dtype=torch.float).to(device)
+        loss_MSI = criterion_MSI(outputs_MSI, label_MSI)
+
+        label_IT = torch.tensor(valid_labels['INJURIES_TOTAL'][i], dtype=torch.float).to(device)
+        loss_IT = criterion_IT(outputs_IT, label_IT)
+
+        total_loss[0] += loss_CT.item()
+        total_loss[1] += loss_D.item()
+        total_loss[2] += loss_MSI.item()
+        total_loss[3] += loss_IT.item()
+
+        predicted_CT = torch.sigmoid(outputs_CT)
+        predicted_D = torch.sigmoid(outputs_D) 
+        predicted_MSI = torch.sigmoid(outputs_MSI)
+        predicted_IT = torch.sigmoid(outputs_IT)
+
+        # print(torch.argmax(label_CT, dim=0))
+
+        correct[0] += (torch.argmax(predicted_CT, dim=0) == torch.argmax(label_CT, dim=0)).sum().item()
+        correct[1] += (torch.argmax(predicted_D, dim=0) == torch.argmax(label_D, dim=0)).sum().item()
+        correct[2] += (torch.argmax(predicted_MSI, dim=0) == torch.argmax(label_MSI, dim=0)).sum().item()
+        correct[3] += (torch.argmax(predicted_IT, dim=0) == torch.argmax(label_IT, dim=0)).sum().item()
+
+avg_loss = [tl / valid_data.shape[0] for tl in total_loss]
+accuracy = [c / valid_data.shape[0] for c in correct]
+
+print("Validation Loss and Accuracy: ")
+print(avg_loss)
+print(accuracy)
+
+total_loss = [0, 0, 0, 0]
+correct = [0, 0, 0, 0]
+
+with torch.no_grad():
+    for i in range(test_data.shape[0]):
+        line = torch.tensor(test_data[i], dtype=torch.float).to(device)
+        outputs_CT = model_CT(line)
+        outputs_D = model_D(line)
+        outputs_MSI = model_MSI(line)
+        outputs_IT = model_IT(line)
+
+        label_CT = torch.tensor(test_labels['CRASH_TYPE'][i], dtype=torch.float).to(device)
+        loss_CT = criterion_CT(outputs_CT, label_CT)
+
+        label_D = torch.tensor(test_labels['DAMAGE'][i], dtype=torch.float).to(device)
+        loss_D = criterion_D(outputs_D, label_D)
+
+        label_MSI = torch.tensor(test_labels['MOST_SEVERE_INJURY'][i], dtype=torch.float).to(device)
+        loss_MSI = criterion_MSI(outputs_MSI, label_MSI)
+
+        label_IT = torch.tensor(test_labels['INJURIES_TOTAL'][i], dtype=torch.float).to(device)
+        loss_IT = criterion_IT(outputs_IT, label_IT)
+
+        total_loss[0] += loss_CT.item()
+        total_loss[1] += loss_D.item()
+        total_loss[2] += loss_MSI.item()
+        total_loss[3] += loss_IT.item()
+
+        predicted_CT = torch.sigmoid(outputs_CT)
+        predicted_D = torch.sigmoid(outputs_D) 
+        predicted_MSI = torch.sigmoid(outputs_MSI)
+        predicted_IT = torch.sigmoid(outputs_IT)
+
+        # print(torch.argmax(label_CT, dim=0))
+
+        correct[0] += (torch.argmax(predicted_CT, dim=0) == torch.argmax(label_CT, dim=0)).sum().item()
+        correct[1] += (torch.argmax(predicted_D, dim=0) == torch.argmax(label_D, dim=0)).sum().item()
+        correct[2] += (torch.argmax(predicted_MSI, dim=0) == torch.argmax(label_MSI, dim=0)).sum().item()
+        correct[3] += (torch.argmax(predicted_IT, dim=0) == torch.argmax(label_IT, dim=0)).sum().item()
+
+avg_loss = [tl / test_data.shape[0] for tl in total_loss]
+accuracy = [c / test_data.shape[0] for c in correct]
+
+print("Test Loss and Accuracy: ")
+print(avg_loss)
+print(accuracy)
